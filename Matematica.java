e26692d5586ae4838c781c13622a149912772757
Júlia Negreiros <93203124+mjulianegreiros@users.noreply.github.com>
2024-11-29 12:08:25 -0300

    q4 monitoria

---
+++ Matematica.java
@@ -0,0 +1,12 @@
+public class Matematica{
+	// atributos
+	public static final double PI = 3.14;
+	
+	// métodos
+	public static double perimetro(double r){
+		return 2*PI*r;
+	}
+	public static double area(double r){
+		return PI*r*r;
+	}
+}
---
+++ Q4.java
@@ -0,0 +1,9 @@
+
+public class Q4{
+	public static void main(String args[]){
+		double p = Matematica.perimetro(10);
+		double a = Matematica.area(10);
+		System.out.println(p);
+		System.out.println(a);
+	}
+}
