5e949d25e5245f8454522c82daf81e24cd0402aa
Júlia Negreiros <93203124+mjulianegreiros@users.noreply.github.com>
2024-12-12 11:27:34 -0300

    Add files via upload

---
+++ ConversorMoeda.java
@@ -0,0 +1,13 @@
+package Q15;
+public class ConversorMoeda{
+	private static double dolar = 5.94;
+	private static double euro = 6.22;
+	
+	public static double realDolar(double real){
+		return real*dolar;
+	}
+	public static double realEuro(double real){
+		return real*euro;
+	}
+	
+}
