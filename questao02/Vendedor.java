5c43b7f8c6d9a2a2791e6c699b2638c59b789787
Júlia Negreiros <93203124+mjulianegreiros@users.noreply.github.com>
2025-01-10 11:46:04 -0300

    ex2 vendedor

---
+++ questao02/Vendedor.java
@@ -0,0 +1,26 @@
+public class Vendedor extends Funcionario{
+	private double comissao;
+
+	public Vendedor(String nome, int idade, double salarioBase , double comissao){
+		super(nome, idade, salario);
+		this.comissao = comissao;
+	}
+	
+
+	public double getComissao(){
+		return this.comissao;
+	}
+	public void setComissao(double comissao){
+		this.comissao = comissao;
+	}
+	
+	@Override
+	public double calcularSalario(){
+		this.setSalarioBase(this.getSalarioBase() + this.getComissao());
+		return getSalarioBase();
+	}
+	public void exibirDetalhes(){
+		super.exibirDetalhes());
+		System.out.println(this.getComissao());
+	}
+}
