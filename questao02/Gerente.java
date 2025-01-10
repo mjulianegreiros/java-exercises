cf2ca20a6907325f76497921e12b231c0a864eab
Júlia Negreiros <93203124+mjulianegreiros@users.noreply.github.com>
2025-01-10 11:45:24 -0300

    ex2 gerente

---
+++ questao02/Gerente.java
@@ -0,0 +1,25 @@
+public class Gerente extends Funcionario{
+	protected String departamento;
+	
+	public Gerente(String nome, int idade, double salarioBase, String departamento){
+		super(nome, idade, salario);
+		this.departamento = departamento
+	}
+
+	public String getDepartamento(){
+		return this.departamento;
+	}
+	public void setDepartamento(String departamento){
+		this.departamento = departamento;
+	}
+
+	@Override
+	public double calcularSalario(){
+		this.setSalarioBase(this.getSalarioBase()*0.2 + this.getSalarioBase());
+		return getSalarioBase();
+	}
+	public void exibirDetalhes(){
+		super.exibirDetalhes();
+		System.out.println(this.getDepartamento());
+	}
+}
